ff234f29e142dd0ba1a05d981173410a8dc33bef
Bayu Pranoto Dwi Putra <bayupranotodwiputra@gmail.com>
2024-10-11 21:27:57 +0700

    commentaries

--- src/helpers/debounce.tsx
+++ src/helpers/debounce.tsx
@@ -3,13 +3,16 @@ import { useState, useEffect } from 'react';
 function useDebounce(value: number, delay: number) {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
+    // update debounced value after delay
     const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
+
+    // cancel the timeout if value changes (also on delay change or unmount)
     return () => {
       clearTimeout(handler);
     };
-  }, [value, delay]);
+  }, [value, delay]); // re-call effect if value or delay changes
   return debouncedValue;
 }
 
